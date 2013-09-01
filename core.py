ea8ceb2e783b37ba9ebd9f45a9bc9eabc281726c
strollhy <strollhuyac@gmail.com>
2013-09-01 11:06:42 -0500

    modify the motion

--- core.py
+++ core.py
@@ -39,9 +39,9 @@ class Core:
         self.tx = self.ty = self.tz = 0
 
         # Start
-        self.__start()
+        self.__game_loop()
 
-    def __start(self):
+    def __game_loop(self):
         while True:
             # Response to event
             self.__on_event()
@@ -61,12 +61,15 @@ class Core:
             self.graphic.render()
 
             # Draw objects
-            self.draw()
+            self.__on_draw()
 
             # Show the screen
             pygame.display.flip()
 
     def __on_event(self):
+
+        self.__get_movement()
+
         for event in pygame.event.get():
             if event.type == QUIT:
                 exit()
@@ -91,44 +94,6 @@ class Core:
             if event.type == KEYUP and event.key == K_6:
                 self.graphic.textId = 5
 
-            ''' For movement '''
-            x, y = self.graphic.sight
-
-            # move forward
-            if event.type == KEYDOWN and event.key == K_w:
-                self.dz = -SPEED * math.cos(math.radians(x))
-                self.dx = SPEED * math.sin(math.radians(x))
-                
-            if event.type == KEYUP and event.key == K_w:
-                self.dz = 0
-                self.dx = 0 
-                
-            # move backward
-            if event.type == KEYDOWN and event.key == K_s:
-                self.dz = SPEED * math.cos(math.radians(x))
-                self.dx = -SPEED * math.sin(math.radians(x))
-
-            if event.type == KEYUP and event.key == K_s:
-                self.dz = 0
-                self.dx = 0
-
-            # move left
-            if event.type == KEYDOWN and event.key == K_a:
-                self.dz = -SPEED * math.cos(math.radians(x - 90))
-                self.dx = SPEED * math.sin(math.radians(x - 90))
-                
-            if event.type == KEYUP and event.key == K_a:
-                self.dz = 0
-                self.dx = 0
-
-            # move right
-            if event.type == KEYDOWN and event.key == K_d:
-                self.dz = -SPEED * math.cos(math.radians(x + 90))
-                self.dx = SPEED * math.sin(math.radians(x + 90))
-                
-            if event.type == KEYUP and event.key == K_d:
-                self.dz = 0
-                self.dx = 0
 
             ''' For sight direction '''
             # look up
@@ -168,8 +133,50 @@ class Core:
                 x, y = self.graphic.sight
                 self.graphic.sight = (x+self.tx, y+self.ty)
                 self.tx = self.ty = 0
+    
+    def __get_movement(self):
 
-    def draw(self):
+        ''' Get movement '''
+        x, y = self.graphic.sight
+
+        keys = pygame.key.get_pressed()
+
+        v_z = [0] * 4
+        v_x = [0] * 4
+        # move forward
+        if keys[K_w]:
+            v_z[0] = -SPEED * math.cos(math.radians(x))
+            v_x[0] = SPEED * math.sin(math.radians(x))
+        else:
+            v_z[0] = v_x[0] = 0
+
+        
+        # move backward
+        if keys[K_s]:
+            v_z[1] = SPEED * math.cos(math.radians(x))
+            v_x[1] = -SPEED * math.sin(math.radians(x))
+        else:
+            v_z[1] = v_x[1] = 0
+            
+        # move left
+        if keys[K_a]:
+            v_z[2] = -SPEED * math.cos(math.radians(x - 90))
+            v_x[2] = SPEED * math.sin(math.radians(x - 90))
+        else:
+            v_z[2] = v_x[2] = 0
+        
+        # move right
+        if keys[K_d]:
+            v_z[3] = -SPEED * math.cos(math.radians(x + 90))
+            v_x[3] = SPEED * math.sin(math.radians(x + 90))
+        else:
+            v_z[3] = v_x[3] = 0
+        
+        # sum the montion vector
+        self.dz = sum(v_z)
+        self.dx = sum(v_x)
+
+    def __on_draw(self):
         # Draw the entire world
         tex = Graphic.genTexcoord(self.graphic.textId)
         for k,m in self.world.map.items():
